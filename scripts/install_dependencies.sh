0a9ec915f5a10fecfe8f399340c73fdb19057e40
Jason Tang <ctang1@andrew.cmu.edu>
2022-04-08 16:59:29 -0400

    changed order of installs

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,9 +5,9 @@ curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
 yum install -y gcc-c++ make
 yum install -y nodejs npm
 
-npm install n -g
-n stable
 npm install -g pm2
 npm install --save express nodemon
 npm install aws-sdk
 npm install 
+npm install n -g
+n stable
