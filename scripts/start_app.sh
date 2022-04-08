8eb071b9ba022c48520897773207fa0aa98361ce
Jason Tang <ctang1@andrew.cmu.edu>
2022-04-07 20:22:59 -0400

    debugging alternate method

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$NODE_ENV" = "production" ]; then
-  node index.js > app.out.log 2> app.err.log < /dev/null & 
+  npm index.js > app.out.log 2> app.err.log < /dev/null & 
 else
-  nodemon --ignore './sessions' index.js;
+  npm --ignore './sessions' index.js;
 fi
