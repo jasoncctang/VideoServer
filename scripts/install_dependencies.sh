eaffcf33d6d10eb52e8d7b6ff2e5e269ddc3c23b
Jason Tang <ctang1@andrew.cmu.edu>
2022-04-08 16:36:34 -0400

    update nodejs

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -5,6 +5,8 @@ curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
 yum install -y gcc-c++ make
 yum install -y nodejs npm
 
+npm install n -g
+n stable
 npm install -g pm2
 npm install --save express nodemon
 npm install aws-sdk
